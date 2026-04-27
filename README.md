4e593b404ee5d294ff49dd206dd5b87df7386c73
Acrylite <187858858+Acrylite@users.noreply.github.com>
2026-04-27 16:39:01 +0700

    [ADD] basic backend authorization service

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# to-do-app-393
+# Bài thực hành số 1
+## I. Thông tin cá nhân
+**Họ và tên:** Võ Sỹ Ngọc
+
+**MSSV:** 24120393
+
+**Email (HCMUS):** 24120393@student.hcmus.edu.vn
+
+**Email (cá nhân):** ngocyoyolove197@gmail.com
+
---
+++ backend/routers/auth.py
@@ -0,0 +1,48 @@
+# heavily reworked from https://github.com/Khoan-IT/chatbot-page/blob/main/backend/app/routers/auth.py
+# to work with React instead of Streamlit
+import os
+
+from fastapi import APIRouter, HTTPException, Depends
+from firebase_admin import auth as admin_auth
+
+from dotenv import load_dotenv
+
+from backend.schemas.auth import GoogleLoginRequest
+from backend.core.firebase_config import init_firebase_admin
+from backend.dependencies.auth import get_current_user
+
+# shortcut
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+# initialize the firebase app
+init_firebase_admin()
+
+# replacing Streamlit secrets with .env file
+load_dotenv()
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+FIREBASE_WEB_API_KEY = os.getenv("FIREBASE_WEB_API_KEY")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+# google login route
+@router.post("/google")
+def google_login(payload: GoogleLoginRequest):
+    try:
+        decoded = admin_auth.verify_id_token(payload.idToken)
+        return {
+            "email": decoded.get("email"),
+            "uid": decoded.get("uid"),
+            "idToken": payload.idToken
+        }
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=f"Google token invalid: {e}")
+
+# get user info route
+@router.get("/me")
+def me(user=Depends(get_current_user)):
+    return {
+        "email": user["email"],
+        "uid": user["uid"]
+    }
